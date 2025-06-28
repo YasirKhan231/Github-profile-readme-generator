39fbab9b00da4745f4828de51d9eb41c3fbd7050
YasirKhan231 <yk4987746@gmail.com>
2025-06-28 13:08:53 +0530

    eslint chaneg

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,21 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    rules: {
+      "react/no-unescaped-entities": "off",
+      "react/jsx-key": "off",
+      "react/react-in-jsx-scope": "off", // Optional for older setups
+      "no-unused-vars": "off",
+      "no-undef": "off",
+      "jsx-a11y/alt-text": "off",
+      "jsx-a11y/anchor-is-valid": "off",
+      "import/no-anonymous-default-export": "off",
+      "@next/next/no-img-element": "off",
+      "@next/next/no-page-custom-font": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
